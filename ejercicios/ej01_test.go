d52ffe0f46c234ab1961aee5c7e14c029007b5b9
martin-albarracin <mafranzone@untref.edu.ar>
2023-05-12 18:58:49 -0300

    Guia actualizada

--- ejercicios/ej01_test.go
+++ ejercicios/ej01_test.go
@@ -8,6 +8,6 @@ func TestDados(t *testing.T) {
 	n, k, x := 3, 6, 7
 	formas := Dados(n, k, x)
 	if formas != 15 {
-		t.Errorf("Dados(n, k, x) = %d; want 15", formas)
+		t.Errorf("Dados(n, k, x) = %d; se esperaba 15", formas)
 	}
 }
